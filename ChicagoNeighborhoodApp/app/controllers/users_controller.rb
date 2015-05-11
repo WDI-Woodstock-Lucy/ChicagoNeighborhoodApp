8b11d698acbd4d8753b51b1cc66a379ab7c1880f
Julia Chrusciel <juliamchrusciel@gmail.com>
2015-05-11 14:19:58 -0500

    Adds user model and users  controller

--- ChicagoNeighborhoodApp/app/controllers/users_controller.rb
+++ ChicagoNeighborhoodApp/app/controllers/users_controller.rb
@@ -1,41 +1,41 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
-  end
+      @users = User.all
+    end
 
-  def new
-    @user = User.new
-  end
+    def new
+      @user = User.new
+    end
 
-  def edit
-    user = User.create(user_params)
-    redirect_to "/users/#{user_id}"
-  end
+    def edit
+      user = User.create(user_params)
+      redirect_to "/users/#{user_id}"
+    end
 
-  def update
-    user = User.find(params[:id])
-    user.update!(user_params)
-    redirect_to "/users/#{user_id}"
-  end
+    def update
+      user = User.find(params[:id])
+      user.update!(user_params)
+      redirect_to "/users/#{user_id}"
+    end
 
-  def show
-    @user = User.find(params[:id])
-  end
+    def show
+      @user = User.find(params[:id])
+    end
 
-  def destroy
-    User.destroy(params[:id])
-    redirect_to "/users"
-  end
+    def destroy
+      User.destroy(params[:id])
+      redirect_to "/users"
+    end
 
-  def profile
-    authenticate!
-    @user = current_user
-    @review = Review.new
-  end
+    def profile
+      authenticate!
+      @user = current_user
+      @review = Review.new
+    end
 
-  private
+    private
 
-  def user_params
-    params.require(:user).permit(:username, :password)
-  end
+    def user_params
+      params.require(:user).permit(:primary_key_id, :username, :password_digest, :email)
+    end
 end
