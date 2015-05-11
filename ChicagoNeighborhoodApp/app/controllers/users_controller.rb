2f8690ff79018b2790accaf2aceb6dd26417c78f
Julia Chrusciel <juliamchrusciel@gmail.com>
2015-05-11 14:03:17 -0500

    Adds user model and controller

---
+++ ChicagoNeighborhoodApp/app/controllers/users_controller.rb
@@ -0,0 +1,41 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+    user = User.create(user_params)
+    redirect_to "/users/#{user_id}"
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update!(user_params)
+    redirect_to "/users/#{user_id}"
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    User.destroy(params[:id])
+    redirect_to "/users"
+  end
+
+  def profile
+    authenticate!
+    @user = current_user
+    @review = Review.new
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+end
