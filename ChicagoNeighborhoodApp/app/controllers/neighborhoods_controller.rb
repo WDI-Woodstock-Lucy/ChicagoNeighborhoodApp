d2368bde13db2d74dd0b15b11c5467ae88835a73
Julia Chrusciel <juliamchrusciel@gmail.com>
2015-05-12 11:50:10 -0500

    Added Favorite model and Favorites Controller

--- ChicagoNeighborhoodApp/app/controllers/neighborhoods_controller.rb
+++ ChicagoNeighborhoodApp/app/controllers/neighborhoods_controller.rb
@@ -20,15 +20,28 @@ class NeighborhoodsController < ApplicationController
     parameters = { term: "bar"}
     results = Yelp.client.search(location_name, parameters)
     @bars = results.businesses.sample(3)
-    
+
   end
 
   def create
     User.create
   end
 
-  def create
-    User.create
+  def profile
+    #Locate profile user
+    profile_user = params[:username, :password]
+    @user = User.find(username, password)
+
+    #Get profile information (saved attractions lists and list of submitted reviews)
+    saved_info = params[:favorite_id]
+    @favorite = Favorite.find(favorite_id)
+
+    @review = Review.find(params(busines))
+
+    #Make changes to profile (add, subtract saved)
+
+    profile_use
+    profile_location = User.
   end
 
 
