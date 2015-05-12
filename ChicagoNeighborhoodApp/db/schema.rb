d14394396643851987df5e9c1451bcc48a57187d
Julia Chrusciel <juliamchrusciel@gmail.com>
2015-05-12 11:57:37 -0500

    Migrates with Favorites models and controllers added

--- ChicagoNeighborhoodApp/db/schema.rb
+++ ChicagoNeighborhoodApp/db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150511191436) do
+ActiveRecord::Schema.define(version: 20150512164422) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer  "favorite_id"
+    t.text     "business_name"
+    t.integer  "business_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
   create_table "neighborhoods", force: :cascade do |t|
     t.string   "name"
@@ -38,9 +46,10 @@ ActiveRecord::Schema.define(version: 20150511191436) do
 
   create_table "users", force: :cascade do |t|
     t.integer  "primary_key_id"
-    t.string   "username"
-    t.string   "password_digest"
-    t.string   "email"
+    t.string   "username",        null: false
+    t.string   "password_digest", null: false
+    t.string   "email",           null: false
+    t.boolean  "is_admin"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
