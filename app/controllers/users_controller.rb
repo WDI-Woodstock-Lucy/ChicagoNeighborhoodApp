13dca76adad74a8955dbcdaa880cdc58422a54bc
Julia Chrusciel <juliamchrusciel@gmail.com>
2015-05-13 11:39:06 -0500

    Creates User directory in Views and profile.html.erb information in the user directory, transfers profile related methods from the neighborhoods controller to the users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,4 +37,30 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:password, :email, :is_admin)
     end
+
+    def profile
+      #Locate profile user
+      authenticate!
+      @user = current_user
+      @useredit = User.new
+    end
+
+    def update_prof
+      #Make changes to profile (add, subtract saved)
+      @user_update_email = @user.update(:email)
+      @user_update_password = @user.update(:password)
+      user_update_email.save!
+      user_update_password.save!
+
+      # id = @user.find(params[:primary_key_id])
+      # email_change = User.find(primary_key_id).edit(:email)
+      # password_change = User.find(primary_key_id).edit(:password)
+    end
+
+    def render_prof_reviews
+      # #Review info - to create, to get all previous reviews and to post
+      # all_reviews = @review.index
+      # new_review = @review.create
+      # delete_review = @review.destroy
+    end
 end
