128b8115ae5677e1dbcf60e7b9ca8317852f5eba
James Conroy <jamespc167@gmail.com>
2015-05-15 15:44:10 -0500

    removes styling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to '/'
+      redirect_to '/admin'
     else
       redirect_to '/'
     end
