8e727363e3fc3e39b2fe3b644221cd2e943b080b
James Conroy <jamespc167@gmail.com>
2015-05-15 14:49:40 -0500

    users are logged in immediately upon sign-up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to '/profile'
+      redirect_to '/'
     else
       redirect_to '/'
     end
