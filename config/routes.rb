8a753fb10819874b772e646a726025847f8dd326
James Conroy <jamespc167@gmail.com>
2015-05-15 11:38:20 -0500

    removes review controller, helper, model, etc

--- config/routes.rb
+++ config/routes.rb
@@ -5,93 +5,32 @@ Rails.application.routes.draw do
   #Neighborhood Routes (Main)
   root 'neighborhoods#index'
 
-
-
-
-  post '/users' => 'users#create'
-
-  post 'sessions' => 'sessions#create'
-  delete 'sessions' => 'sessions#destroy'
-
-  get '/search/:id' => 'neighborhoods#search'
-
-  get '/admin' => 'admin#home'
-  patch 'admin' => 'admin#update'
-  post '/admin' => 'admin#create'
-  delete '/admin' => 'admin#destroy'
-
-#Profile routes (to get profile, update information)
-  get '/profile' => 'users#profile'
-
-  post '/profile' => 'users#update_prof'
-
-  get '/users/:id' => 'users#profile'
-
-  resources :users, only: [:update, :create]
-
-  get '/reviews' => 'reviews#index'
-
-  post '/reviews' => 'reviews#create'
-
-  delete '/reviews/:id' => 'reviews#destroy'
-
+  #Neighborhood Routes
   resources :neighborhoods, except: [:edit, :update]
   get '/neighborhoods/:id/edit' => 'admin#home'
   patch '/neighborhoods/:id' => 'admin#update'
   put '/neighborhoods/:id' => 'admin#update'
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  # User Routes
+  resources :users, only: [:update, :create]
+  post '/users' => 'users#create'
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  # Sessions Routes
+  post 'sessions' => 'sessions#create'
+  delete 'sessions' => 'sessions#destroy'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  # Search Routes
+  get '/search/:id' => 'neighborhoods#search'
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  # Admin Routes
+  get '/admin' => 'admin#home'
+  patch 'admin' => 'admin#update'
+  post '/admin' => 'admin#create'
+  delete '/admin' => 'admin#destroy'
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  #Profile routes (to get profile, update information)
+  get '/profile' => 'users#profile'
+  post '/profile' => 'users#update_prof'
+  get '/users/:id' => 'users#profile'
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
